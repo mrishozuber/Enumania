98e25ee619106d920c1ef25aeef3e848dc644e63
mrishozuber <mrishozuber@gmail.com>
2022-05-01 00:19:35 +0000

    Create Enumania.py

---
+++ Enumania.py
@@ -0,0 +1,11 @@
+import requests
+import sys
+import socket
+import pyfiglet
+
+
+ascii_banner = pyfiglet.figlet_format("Enumania")
+print(ascii_banner)
+print("                                           V1.0 \nDeveloped by @tzanonima  twitter/facebook/instagram @tzanonima \n")
+choice =int(input("Choose your choice \n \n1. Scanning \n2. Subdomain \n3. Directory \n4. Email Havester \n5. Social account finder \n>
+
